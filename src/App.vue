15aea230ce996e6e0c771c701df155b78b562dcd
kyle <1162347229@qq.com>
2020-04-08 10:35:07 +0800

    路由渲染不同的router-view

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <ul>
+      <li><router-link to="/" name="view1">首页</router-link></li>
+      <li><router-link to="/about" name="view2">About</router-link></li>
+    </ul>
+    <div>
+      <h1>view</h1>
+      <router-view></router-view>
     </div>
-    <router-view/>
+    <div>
+      <h1>view1</h1>
+      <router-view name="view1"></router-view>
+    </div>
+    <div>
+      <h1>view2</h1>
+      <router-view name="view2"></router-view>
+    </div>
+
+
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import About from '../views/About.vue'
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    // 路径为/的时候，router-view 渲染对应的组件 根据 router-view name属性来渲染；如 <router-view name="view1" />
+    components: {
+      default: Home,
+      view1: Home, // router-view --name 为 view1 渲染 Home
+      view2: About
+    }
   },
   {
     path: '/about',
